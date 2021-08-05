830ef0f9f08b08708d2896af0894440f68e8794c
mauro-titanio <maurotitanio@gmail.com>
2021-08-05 12:56:58 +0200

    Update MoviesGrid.jsx

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -2,6 +2,7 @@ import movies from "../movies.json";
 import { MovieCard } from "./MovieCard";
 import styles from "./MoviesGrid.module.css"
 export function MoviesGrid() {
+  
   console.log(movies);
   return (
     <ul className={styles.moviesGrid}>
